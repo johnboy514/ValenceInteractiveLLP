d11c51a29d3fa90752c5db19b80d697a1c5eeb75
rjo6615 <robertjobernier@gmail.com>
2024-06-07 17:29:16 -0400

    removed message us button and added functionality to contact us link

--- src/components/Home.js
+++ src/components/Home.js
@@ -503,7 +503,7 @@ function Home() {
     </div>
   </div>
 </div>
-<section class="">
+<section id="contact">
   <div class="px-4 py-5 px-md-5 text-center text-lg-start" style={{backgroundColor: "hsl(0, 0%, 96%)"}}>
     <div class="container">
       <div class="row gx-lg-5 align-items-center">
